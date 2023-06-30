68f17e661a82dc7a7e9c713b99f6ebde05478793
saarryp <s.rypkema@casema.nl>
2023-06-30 15:41:23 +0200

    nav gemaakt en begin homepage

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import "./Homepage.css";
-// import ImageGil from "./assets/GilScottHeron.jpg";
+import "./HomePage.css";
+import gil from "../../assets/gil-scott-heron.jpg";
+
+
+
 
 
 
 export default function Home() {
     return (
+        <>
         <div>
-            <h1>dit is de homepage</h1>
-            {/*<img src={ImageGil} alt="musician Gil Scott-Heron"/>*/}
+            <h2>dit is de homepage</h2>
+            <img src= {gil} alt= "musician Gil Scott-Heron" className="image-resize"/>
         </div>
+            <article className="homepage-text">
+                <p>Are you bored of that sames shizzle again?</p>
+
+                <p>Is a streaming service sending you the same 'ole same 'ole?</p>
+                <p>Are you into souls etc</p>
+                <p>are you getting lost and lonely</p>
+            </article>
+
+            </>
     )
 }
