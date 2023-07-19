2efdb9c93da3fcc77718f335aca0271b416e6d10
saarryp <s.rypkema@casema.nl>
2023-07-19 14:10:36 +0200

    api titel, artiest en albumcover kunnen opvragen

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -14,9 +14,10 @@ export default function Search() {
         e.preventDefault()
 
         try {
-            const url = `https://api.discogs.com/database/search?q=${query}&token=${token}&{?type,artist,title}`;
+            const url = `https://api.discogs.com/database/search?q=${query}&token=${token}&{?type,artist,title,thumb}`;
             const response = await axios.get(url)
             setArtists(response.data.results)
+            console.log(response.data)
         } catch (err) {
             console.log(err)
         }
@@ -24,7 +25,8 @@ export default function Search() {
 
 
     // op andeer manier het object van de discogs inzetten waarbij je waarschijnlijk onder "thumb" te vinden is zie body
-    // status{
+    // statusnpm start
+    // {
     //   "pagination": {
     //     "per_page": 50,
     //     "items": 9,
@@ -33,30 +35,8 @@ export default function Search() {
     //     "pages": 1
     //   },
     //   "releases": [
-    //     {
-    //       "artist": "Nickelback",
-    //       "id": 173765,
-    //       "main_release": 3128432,
-    //       "resource_url": "http://api.discogs.com/masters/173765",
-    //       "role": "Main",
-    //       "thumb": "https://api-img.discogs.com/lb0zp7--FLaRP0LmJ4W6DhfweNc=/fit-in/90x90/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-5557864-1396493975-7618.jpeg.jpg",
-    //       "title": "Curb",
-    //       "type": "master",
-    //       "year": 1996
-    //     },
-    //     {
-    //       "artist": "Nickelback",
-    //       "format": "CD, EP",
-    //       "id": 4299404,
-    //       "label": "Not On Label (Nickelback Self-released)",
-    //       "resource_url": "http://api.discogs.com/releases/4299404",
-    //       "role": "Main",
-    //       "status": "Accepted",
-    //       "thumb": "https://api-img.discogs.com/eFRGD78N7UhtvRjhdLZYXs2QJXk=/fit-in/90x90/filters:strip_icc():format(jpeg):mode_rgb()/discogs-images/R-4299404-1361106117-3632.jpeg.jpg",
-    //       "title": "Hesher",
-    //       "type": "release",
-    //       "year": 1996
-    //     },
+
+    //
     //     {
     //       "artist": "Nickelback",
     //       "id": 173767,
@@ -82,10 +62,12 @@ export default function Search() {
                     <button className="search-button" type="submit">Search</button>
                 </form>
             <div>
-                <ul>
-                    {artists.map((artist) => (
-                        <li key={artist.id}>{artist.title}</li>
-                    ))}
+                <ul className="ul-position">
+                   {artists.map((artist) =>
+                       <li key={artist.id}><strong>{artist.title}</strong>
+                            {artist.thumb && <img src={artist.thumb} alt={artist.title}/>}
+                     </li>
+                  )}
                 </ul>
             </div>
           </div>
