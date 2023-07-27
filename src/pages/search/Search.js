9516d787c32d2d19ae7c2585f6266641f22d9f55
saarryp <s.rypkema@casema.nl>
2023-07-27 15:01:57 +0200

    slider component gemaakt en toegevoegd

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,6 +1,7 @@
 import "./Search.css";
 import React, { useState } from "react";
 import axios from "axios";
+import Spinner from "../../components/spinner/Spinner";
 
 export default function Search() {
     const [query, setQuery] = useState('');
@@ -9,6 +10,7 @@ export default function Search() {
             album: [],
         },
     });
+    const [loading,setLoading] = useState(false);
 
     const fetchAlbumDetails = async (mbid) => {
         try {
@@ -24,13 +26,13 @@ export default function Search() {
 
     let handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true);
 
         try {
             const token = process.env.REACT_APP_LASTFM_TOKEN;
             const url = `http://ws.audioscrobbler.com/2.0/?method=album.search&album=${query}&api_key=${token}&format=json`;
             const response = await axios.get(url);
 
-            // Filter out duplicate albums based on mbid
             const uniqueAlbums = [];
             const uniqueMbidSet = new Set();
             for (const album of response.data.results.albummatches.album) {
@@ -47,37 +49,14 @@ export default function Search() {
             setResults({ albummatches: { album: uniqueAlbums } });
         } catch (err) {
             console.log(err);
+        } finally {
+            setLoading(false);
         }
     };
 
-    // let handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //
-    //     try {
-    //         const token = process.env.REACT_APP_LASTFM_TOKEN;
-    //         const url = `http://ws.audioscrobbler.com/2.0/?method=album.search&album=${query}&api_key=${token}&format=json`;
-    //         const response = await axios.get(url);
-    //
-    //         // Filter out duplicate albums based on mbid
-    //         const uniqueAlbums = [];
-    //         const uniqueMbidSet = new Set();
-    //         response.data.results.albummatches.album.forEach((album) => {
-    //             if (!uniqueMbidSet.has(album.mbid)) {
-    //                 uniqueMbidSet.add(album.mbid);
-    //                 uniqueAlbums.push(album);
-    //             }
-    //         });
-    //
-    //         setResults({ albummatches: { album: uniqueAlbums } });
-    //         console.log(response.data);
-    //     } catch (err) {
-    //         console.log(err);
-    //     }
-    // };
 
     const handleAlbumClick = async (mbid) => {
         const albumDetails = await fetchAlbumDetails(mbid);
-        // Here, you can do something with the albumDetails, such as displaying them in a modal or a separate component
         console.log(albumDetails);
     };
 
@@ -85,6 +64,7 @@ export default function Search() {
         <div className="container-photo-search-engine">
             <div className="position-button-and-results">
                 <form className="form-search-size" onSubmit={handleSubmit}>
+                    <div className="spinner-container">
                     <input className="input-field"
                         type="text"
                         value={query}
@@ -93,8 +73,12 @@ export default function Search() {
                     <button className="search-button" type="submit">
                         Search
                     </button>
+                    </div>
                 </form>
                 <div>
+                    {loading ? (
+                        <Spinner />
+                    ) : (
                     <ul className="ul-position ul-albums">
                         {results.albummatches.album.map((album) => (
                             <li key={`${album.mbid}-${album.name}`} onClick={() => handleAlbumClick(album.mbid)}>
@@ -112,32 +96,12 @@ export default function Search() {
                                         ))}
                                     </ul>
                                 ) : (
-                                    <p>No tracks found for this album.</p>
+                                    <p className="tracks-not-found">No tracks found for this album.</p>
                                 )}
                             </li>
                         ))}
-
-                        {/*{results.albummatches.album.map((album) => (*/}
-                        {/*    <li key={`${album.mbid}-${album.name}`}  onClick={() => handleAlbumClick(album.mbid)}>*/}
-                        {/*        <span className="position-album">*/}
-                        {/*            {album.image && <img src={album.image[3]["#text"]} alt={album.name} />}*/}
-                        {/*        </span>*/}
-                        {/*        <h2 className="artist">{album.artist}</h2>*/}
-                        {/*        <p className="title album-name">{album.name}</p>*/}
-                        {/*        {album.tracks && album.tracks.length > 0 ? (*/}
-                        {/*            <ul className="track-list">*/}
-                        {/*                {album.tracks.map((track) => (*/}
-                        {/*                    <li key={track.name}>*/}
-                        {/*                        <a href={track.url} target="_blank" rel="noreferrer" className="track-name"> {track.name}</a>*/}
-                        {/*                    </li>*/}
-                        {/*                ))}*/}
-                        {/*            </ul>*/}
-                        {/*        ) : (*/}
-                        {/*            <p className="no-tracks">No tracks found for this album.</p>*/}
-                        {/*        )}*/}
-                        {/*    </li>*/}
-                        {/*))}*/}
                     </ul>
+                    )}
                 </div>
             </div>
         </div>
