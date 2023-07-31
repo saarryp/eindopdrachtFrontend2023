a7ad15b85426f9016ba7497a99b58e32cd28fce7
saarryp <s.rypkema@casema.nl>
2023-07-31 15:10:43 +0200

    tracks naar afspeelllijst user gemaakt

--- src/pages/mysounds/MySounds.js
+++ src/pages/mysounds/MySounds.js
@@ -1,26 +1,38 @@
 import "./MySounds.css"
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 export default function MySounds() {
-    return (
-    <>
-        <div className="background-container">
-            <ol>
-                <li className='list-items'> 1</li>
-                <li className="list-items"> 2</li>
-                <li className='list-items'> 3</li>
-                <li className='list-items'> 4</li>
-                <li className='list-items'> 5</li>
-                <li className='list-items'> 6</li>
-                <li className='list-items'> 7</li>
-                <li className='list-items'> 8</li>
-                <li className='list-items'> 9</li>
-                <li className='list-items'> 10</li>
-            </ol>
-            <div className="position-button">
-                <button className="button-logout">uitloggen</button>
+    const [myFavorites, setMyFavorites] = useState('');
+
+    useEffect(() => {
+        const storedFavorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+        setMyFavorites(storedFavorites);
+    }, []);
+
+    const handleRemoveFavorite = (song) => {
+        const updatedFavorites = myFavorites.filter((item) => item !== song);
+        setMyFavorites(updatedFavorites);
+        localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+    };
+
+        return (
+        <>
+            <div className="background-container">
+                <ol>
+                    {myFavorites.length > 0 ? (
+                        myFavorites.map((favorite, index) => (
+                            <li key={index} className="list-items">
+                                {favorite.name} <button onClick={() => handleRemoveFavorite(favorite)}>delete</button>
+                            </li>
+                        ))
+                    ) : (
+                        <p className="list-items">No favorites found.</p>
+                    )}
+                </ol>
+                <div className="position-button">
+                    <button className="button-logout">uitloggen</button>
+                </div>
             </div>
-        </div>
-    </>
-    )
-}
+        </>
+        );
+        }
--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,5 +1,6 @@
+
 import "./Search.css";
-import React, { useState,} from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import Spinner from "../../components/spinner/Spinner";
 
@@ -11,12 +12,11 @@ export default function Search() {
         },
     });
     const [loading,setLoading] = useState(false);
-    // const [likedTracks, setLikedTracks] = useState([])
 
     const fetchAlbumDetails = async (mbid) => {
         try {
             const token = process.env.REACT_APP_LASTFM_TOKEN;
-             const url = `http://ws.audioscrobbler.com/2.0/?method=album.getinfo&mbid=${mbid}&api_key=${token}&format=json`;
+            const url = `http://ws.audioscrobbler.com/2.0/?method=album.getinfo&mbid=${mbid}&api_key=${token}&format=json`;
             const response = await axios.get(url);
             return response.data;
         } catch (err) {
@@ -25,16 +25,35 @@ export default function Search() {
         }
     };
 
-    // useEffect(() => {
-    //     const storedLikedSongs = localStorage.getItem('likedTracks);
-    //     if (storedLikedSongs){
-    //         setLikedTracks(JSON.parse(storedLikedSongs));
-    //     }
-    // }, []);
-    //
-    // useEffect(() => {
-    //     localStorage.setItem('likedTracks', JSON.stringify(likedTracks));
-    // }, [likedTracks]);
+    const favoriteSong = (song) => {
+        console.log(song);
+
+        // Get the current liked songs from localStorage
+        const currentFavorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+
+        // Check if the song is already in the liked songs list
+        const isSongLiked = currentFavorites.includes(song);
+
+        // Check if the number of favorites exceeds 10
+        if (currentFavorites.length >= 10) {
+            alert("You have more than 10 favorite songs. Delete song in your page");
+            return;
+        }
+
+        let updatedFavorites;
+        if (isSongLiked) {
+            updatedFavorites = currentFavorites.filter((item) => item !== song);
+        } else {
+            updatedFavorites = [...currentFavorites, song];
+        }
+
+        // Update the liked songs in localStorage
+        localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+        console.log(updatedFavorites);
+    };
+
+
+
 
 
     let handleSubmit = async (e) => {
@@ -79,49 +98,224 @@ export default function Search() {
             <div className="position-button-and-results">
                 <form className="form-search-size" onSubmit={handleSubmit}>
                     <div className="spinner-container">
-                    <input className="input-field"
-                        type="text"
-                        value={query}
-                        onChange={(e) => setQuery(e.target.value)}
-                    />
-                    <button className="search-button" type="submit">
-                        Search
-                    </button>
+                        <input className="input-field"
+                               type="text"
+                               value={query}
+                               onChange={(e) => setQuery(e.target.value)}
+                        />
+                        <button className="search-button" type="submit">
+                            Search
+                        </button>
                     </div>
                 </form>
                 <div>
                     {loading ? (
                         <Spinner />
                     ) : (
-                    <ul className="ul-position ul-albums">
-                        {results.albummatches.album.map((album) => (
-                            <li key={`${album.mbid}-${album.name}`} onClick={() => handleAlbumClick(album.mbid)}>
-                                <div className="album-info">
+                        <ul className="ul-position ul-albums">
+                            {results.albummatches.album.map((album) => (
+                                <li key={`${album.mbid}-${album.name}`} onClick={() => handleAlbumClick(album.mbid)}>
+                                    <div className="album-info">
                                     <span>
                                     {album.image && <img src={album.image[3]["#text"]} alt={album.name} />}
                                     </span>
-                                    <h2 className="artist">{album.artist}</h2>
-                                    <p className="title">{album.name}</p>
-                                </div>
+                                        <h2 className="artist">{album.artist}</h2>
+                                        <p className="title">{album.name}</p>
+                                    </div>
                                     {album.tracks && album.tracks.length > 0 ? (
-                                    <ul className="track-list">
-                                        {album.tracks.map((track) => (
-                                            <li key={track.name}> <a href={track.url} target="_blank" rel="noreferrer"> {track.name}</a></li>
-                                        ))}
-                                    </ul>
-                                ) : (
-                                    <p className="tracks-not-found">No tracks found for this album.</p>
-                                )}
-                            </li>
-                        ))}
-                    </ul>
+                                        <ul className="track-list">
+                                            {album.tracks.map((track) => (
+                                                <>
+                                                <li key={track.name}> <a href={track.url} target="_blank" rel="noreferrer"> {track.name}</a></li>
+                                                <div>
+                                                <button onClick={() => favoriteSong(track)}>
+                                                    like
+                                                </button>
+                                                </div>
+                                                </>
+                                            ))}
+                                        </ul>
+                                    ) : (
+                                        <p className="tracks-not-found">No tracks found for this album.</p>
+                                    )}
+                                </li>
+                            ))}
+                        </ul>
                     )}
                 </div>
             </div>
         </div>
     );
 }
-
-
-
-
+// import "./Search.css";
+// import React, { useState, useEffect} from "react";
+// import axios from "axios";
+// import Spinner from "../../components/spinner/Spinner";
+//
+// export default function Search() {
+//     const [query, setQuery] = useState('');
+//     const [results, setResults] = useState({
+//         albummatches: {
+//             album: [],
+//         },
+//     });
+//     const [loading,setLoading] = useState(false);
+//     const [heartedSongs, setHeartedSongs] = useState([])
+//
+//     const fetchAlbumDetails = async (mbid) => {
+//         try {
+//             const token = process.env.REACT_APP_LASTFM_TOKEN;
+//              const url = `http://ws.audioscrobbler.com/2.0/?method=album.getinfo&mbid=${mbid}&api_key=${token}&format=json`;
+//             const response = await axios.get(url);
+//             return response.data;
+//         } catch (err) {
+//             console.log(err);
+//             return null;
+//         }
+//     };
+//
+//     // const handleHeartClick = (trackName) => {
+//     //     if (heartedSongs.includes(trackName)) {
+//     //         setHeartedSongs(heartedSongs.filter((name) => name !== trackName));
+//     //     } else {
+//     //         setHeartedSongs([...heartedSongs, trackName]);
+//     //     }
+//     // };
+//
+//     const handleHeartClick = (albumIndex, trackIndex) => {
+//         const updatedResults = { ...results };
+//         const trackName = updatedResults.albummatches.album[albumIndex].tracks[trackIndex].name;
+//
+//         if (heartedSongs.includes(trackName)) {
+//             setHeartedSongs(heartedSongs.filter((name) => name !== trackName));
+//             updatedResults.albummatches.album[albumIndex].tracks[trackIndex].hearted = false;
+//         } else {
+//             setHeartedSongs([...heartedSongs, trackName]);
+//             updatedResults.albummatches.album[albumIndex].tracks[trackIndex].hearted = true;
+//         }
+//
+//         setResults(updatedResults);
+//     };
+//
+//
+//
+//
+//     let handleSubmit = async (e) => {
+//         e.preventDefault();
+//         setLoading(true);
+//
+//         try {
+//             const token = process.env.REACT_APP_LASTFM_TOKEN;
+//             const url = `http://ws.audioscrobbler.com/2.0/?method=album.search&album=${query}&api_key=${token}&format=json`;
+//             const response = await axios.get(url);
+//
+//             const uniqueAlbums = [];
+//             const uniqueMbidSet = new Set();
+//             for (const album of response.data.results.albummatches.album) {
+//                 if (!uniqueMbidSet.has(album.mbid)) {
+//                     uniqueMbidSet.add(album.mbid);
+//                     const albumDetails = await fetchAlbumDetails(album.mbid);
+//                     if (albumDetails && albumDetails.album && albumDetails.album.tracks.track && Array.isArray (albumDetails.album.tracks.track)) {
+//                         album.tracks = albumDetails.album.tracks.track.map((track) => ({...track,
+//                         hearted: heartedSongs.includes(track.name),
+//                         }));
+//                     } else {
+//                         album.tracks = [];
+//                     }
+//                     uniqueAlbums.push(album);
+//                 }
+//             }
+//
+//             setResults({ albummatches: { album: uniqueAlbums } });
+//             setQuery('')
+//         } catch (err) {
+//             console.log(err);
+//         } finally {
+//             setLoading(false);
+//         }
+//     };
+//
+//
+//     const handleAlbumClick = async (mbid) => {
+//         const albumDetails = await fetchAlbumDetails(mbid);
+//         console.log(albumDetails);
+//     };
+//
+//     useEffect(() => {
+//         const storedHeartedSongs = JSON.parse(localStorage.getItem('heartedSongs') || '[]');
+//         setHeartedSongs(storedHeartedSongs);
+//
+//         // Initialize the hearted property of each track in the state
+//         const updatedResults = { ...results };
+//         updatedResults.albummatches.album.forEach((album) => {
+//             album.tracks.forEach((track) => {
+//                 track.hearted = storedHeartedSongs.includes(track.name);
+//             });
+//         });
+//         setResults(updatedResults);
+//     }, []);
+//
+//
+//
+//     return (
+//         <div className="container-photo-search-engine">
+//             <div className="position-button-and-results">
+//                 <form className="form-search-size" onSubmit={handleSubmit}>
+//                     <div className="spinner-container">
+//                     <input className="input-field"
+//                         type="text"
+//                         value={query}
+//                         onChange={(e) => setQuery(e.target.value)}
+//                     />
+//                     <button className="search-button" type="submit">
+//                         Search
+//                     </button>
+//                     </div>
+//                 </form>
+//                 <div>
+//                     {loading ? (
+//                         <Spinner />
+//                     ) : (
+//                     <ul className="ul-position ul-albums">
+//                         {results.albummatches.album.map((album, albumIndex) => (
+//                             <li key={`${album.mbid}-${albumIndex}`} onClick={() => handleAlbumClick(album.mbid)}>
+//                                 <div className="album-info">
+//                                     <span>
+//                                     {album.image && <img src={album.image[3]["#text"]} alt={album.name} />}
+//                                     </span>
+//                                     <h2 className="artist">{album.artist}</h2>
+//                                     <p className="title">{album.name}</p>
+//                                 </div>
+//                                     {album.tracks && album.tracks.length > 0 ? (
+//                                     <ul className="track-list">
+//                                         {album.tracks.map((track, trackIndex) => (
+//                                             <li key={track.name}>
+//                                                 <a href={track.url}
+//                                                    target="_blank"
+//                                                    rel="noreferrer"> {track.name}
+//                                                 </a>
+//                                                 <span
+//                                                     className={`heart-icon ${track.hearted ? 'hearted' : ''}`}
+//                                                     onClick={() => handleHeartClick(albumIndex, trackIndex)}
+//                                                 >
+//                                                 ❤️
+//                                                  </span>
+//                                             </li>
+//                                         ))}
+//                                     </ul>
+//                                 ) : (
+//                                     <p className="tracks-not-found">No tracks found for this album.</p>
+//                                 )}
+//                             </li>
+//                         ))}
+//                     </ul>
+//                     )}
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+//
+//
+//
+//
