566fe5a8da53225ca5d339b8cef50f1307b35921
saarryp <s.rypkema@casema.nl>
2023-09-02 19:30:34 +0200

    like-button user bij Search aangepast aan linkerkant net iets meer ruimte van tracknummer.

--- src/helper/checkTokenValidity.js
+++ src/helper/checkTokenValidity.js
@@ -2,7 +2,6 @@ import jwt_decode from "jwt-decode";
 
 
 export function checkTokenValidity(token) {
-
     const decodedToken = jwt_decode(token)
     const expirationTime = decodedToken.exp * 1000;
     const isExpired = Date.now() > expirationTime;
--- src/hooks/useLoginHook.js
+++ src/hooks/useLoginHook.js
@@ -2,12 +2,6 @@ import {useContext} from 'react';
 import axios from "axios";
 import {AuthContext} from "../context/AuthContext";
 
-
-
-//bij inloggen heoft geen token mee te
-// sturen pas na inloggen dat we een token krijgen
-//dan pas token meesturen
-
 function useLoginHook(user, password) {
 
      const {loginFunction} = useContext(AuthContext);
@@ -25,11 +19,6 @@ function useLoginHook(user, password) {
 
             }
             );
-            // console.log(response.data.accesToken)
-            // als de token is opgehaald in de console dan console.log(response.data.accesToken);
-            //dan logInfunction(response.data.accesToken); om de data naar de context door te sturen
-            //dan in de context van AuthContext de functie een argument mee te geven
-            //roep de loginfunctie van de context (loginFunction)aan zodat de rest geregeld kan worden
 
             console.log('Token', response.data.accessToken );
 
