c75a1ea8c6c63c4c5979b331f293c4048106f06e
saarryp <s.rypkema@casema.nl>
2023-09-02 10:34:51 +0200

    ADMIN-PAGE: positie register-button aangepast

--- src/pages/admin/AdminPage.js
+++ src/pages/admin/AdminPage.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import SubscribeModal from "../../components/subscribeModal/SubscribeModal";
+import "./AdminPage.css";
 
 function AdminLoginPage() {
     const [modalOpen, setModalOpen] = useState(false);
@@ -11,13 +12,15 @@ function AdminLoginPage() {
 
     return (
 
-        <div className="container-subscribe-button">
+        <div className="container-admin">
+            <div className="center-button">
             <button
                 onClick={openModal}
                 className={`subscribe-button ${modalOpen ? 'active' : ''}`}
             >
                Admin Register
             </button>
+            </div>
             {modalOpen && <SubscribeModal role={["admin"]} closeModal={() => setModalOpen(false)} />}
         </div>
     );
