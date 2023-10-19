642882b73250fc79e1829c308ecca2fd9cccb7c4
saarryp <s.rypkema@casema.nl>
2023-10-19 14:26:24 +0200

    wanneer scrollen op nummer wordt de tekst groter, zodat gebruiker ziet waar hij is. Tevens een link gemaakt om het nummer te beluisteren.

--- src/components/similarTracksModal/SimilarTracksModal.js
+++ src/components/similarTracksModal/SimilarTracksModal.js
@@ -2,9 +2,14 @@ import React from "react";
 import './SimilarTracksModal.css';
 
 
+
+
 const SimilarTracksModal = ({isOpen, similarTracks, onClose, selectedTrack}) => {
-    if (!isOpen) return null;
-console.log(similarTracks, isOpen);
+
+    if (!isOpen) {return null;
+
+}
+    console.log(similarTracks, isOpen);
 
 
 
@@ -13,13 +18,23 @@ console.log(similarTracks, isOpen);
             <div className="modal-content-similar-tracks">
                 <span className="close-similar-tracks" onClick={onClose}>&times;</span>
                 <h2 className= "border-header">Similar Tracks for
-                    <p className= "similar-track-header ">{selectedTrack.name}  {selectedTrack.artist.name}
-                    </p></h2>
+                    <p className= "similar-track-header ">{selectedTrack.name} by {selectedTrack.artist.name}
+                    </p>
+                </h2>
                 <ul>
                     {similarTracks.map((track, index) => (
                         <li className="list-items-similar" key={index}>
-                            <p><span className="bold-text"> Track: </span> {track.name}</p>
-                            <p><span className="bold-text"> Artist: </span>{track.artist.name}</p>
+                            <div className="scroll-container">
+                            <p>
+                                <span className="bold-text">
+                                    Track:
+                                </span> <a href={track.url} target = "_blank" rel="noopener noreferrer" style={{ color: "black"}}>{track.name}</a>
+                            </p>
+                            <p><span className="bold-text">
+                                Artist:
+                            </span>{track.artist.name}
+                            </p>
+                            </div>
                         </li>
                     ))}
                 </ul>
