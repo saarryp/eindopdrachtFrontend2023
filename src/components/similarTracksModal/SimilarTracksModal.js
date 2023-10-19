b569a34a6eafdf2b51c85ffd617b945488ff5ba8
saarryp <s.rypkema@casema.nl>
2023-10-19 11:41:12 +0200

    modal aangepast met css, track & artist in aparte blokjes en gechecked of deze randomize. Dat is gelukt

--- src/components/similarTracksModal/SimilarTracksModal.js
+++ src/components/similarTracksModal/SimilarTracksModal.js
@@ -1,22 +1,25 @@
 import React from "react";
 import './SimilarTracksModal.css';
 
+
 const SimilarTracksModal = ({isOpen, similarTracks, onClose, selectedTrack}) => {
     if (!isOpen) return null;
 console.log(similarTracks, isOpen);
+
+
 
     return (
         <div className="modal-similar-tracks">
             <div className="modal-content-similar-tracks">
                 <span className="close-similar-tracks" onClick={onClose}>&times;</span>
-                <h2>Similar Tracks for:
-                    <p>{selectedTrack.name}  {selectedTrack.artist.name}
+                <h2 className= "border-header">Similar Tracks for
+                    <p className= "similar-track-header ">{selectedTrack.name}  {selectedTrack.artist.name}
                     </p></h2>
                 <ul>
                     {similarTracks.map((track, index) => (
-                        <li key={index}>
-                            <p>Track: {track.name}</p>
-                            <p>Artist: {track.artist.name}</p>
+                        <li className="list-items-similar" key={index}>
+                            <p><span className="bold-text"> Track: </span> {track.name}</p>
+                            <p><span className="bold-text"> Artist: </span>{track.artist.name}</p>
                         </li>
                     ))}
                 </ul>
